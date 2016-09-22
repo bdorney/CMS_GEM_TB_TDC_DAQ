940e14d2f2a9617c6f05984682a9b93cc217dd94
Brian <brian.l.dorney@cern.ch>
2016-09-22 14:50:01 +0200

    Small cleanup for IDaqVmeInterface

--- include/IDaqVmeInterface.h
+++ include/IDaqVmeInterface.h
@@ -5,77 +5,104 @@
 //#include "CAENVMElib.h"
 
 class IDaqVmeInterface {
-	public:
-		IDaqVmeInterface( IDaqVmeModuleType aModule = idmV2718, short aLink = 0, short aBoardNum = 0, long aBaseAddress = 0 );
-		virtual ~IDaqVmeInterface();
-		
-		//Base Address
-		long GetBaseAddress(){ return baseAddress; };
-		void SetBaseAddress( long aBaseAddress ){ baseAddress = aBaseAddress; };
-		//Reset I/F
-		void Reset();
-		
-		//Link
-		void setBoardNumber( short aBoardNum = 0 ){ boardNum = aBoardNum; return; };
-		void setLinkNumber( short aLink = 0) { link = aLink; return; };
-		void setModuleType( IDaqVmeModuleType aModule = idmV2718 ){ module = aModule; return; };
-
-		// Read functions
-        virtual void Read( uint32_t aAddress, uint32_t &aData);
-        virtual void Read( uint32_t aAddress, uint16_t &aData);
-        virtual void ReadBLT( uint32_t aAddress, uint16_t &aData, const uint16_t nData, uint16_t &nDataRead){};
-        virtual void ReadBLT( uint32_t aAddress, uint32_t &aData, const uint16_t nData, uint16_t &nDataRead);
-        virtual void ReadBLT( uint32_t aAddress, uint64_t &aData, const uint16_t nData, uint16_t &nDataRead);
-		
-		// Write functions
-        virtual void Write( uint32_t aAddress, const uint32_t &aData);
-        virtual void Write( uint32_t aAddress, const uint16_t &aData);
-        virtual void WriteA24(uint32_t, const uint16_t&);
-		//Status
-		virtual IDaqVmeCode GetStatus(){ return status; };
-        virtual void ResetStatus(){ status = IDaqSuccess; };
-  
-		//Connection
-		virtual void Connect();
-        virtual void Disconnect();
-		
-		//Registers
-		virtual void CheckRegister( CVRegisters reg );
-		
-		virtual void SetOutputLevel( int aOutput, IDaqSwitch sw );
-		virtual void CheckOuputConf( int aOutput );
-		virtual void SetOutputSource( int aOutput, CVIOSources s);
-	
-		virtual void SetScalerConf();
-		virtual void ResetScaler();
-		virtual void CheckScaler();
-		virtual unsigned short GetScaler(){ return scaler; };
-		virtual void EnableScaler();
-		virtual void DisableScaler();
-		
-		virtual uint16_t GetInputLevel( int aInput ) { return inputLevel[ aInput ]; }
-        virtual void CheckInputLevel();
-		virtual void SetInputConf( int aInput, bool aPolarity );
-		virtual void GetInputConf( int aInput, int &aPol, int &aLedPol ){ aPol = inPol[ aInput ]; aLedPol = inLedPol[ aInput ]; };
-		virtual void CheckInputConf( int aInput = 0 );
-		
-  	
-	private:
-		IDaqVmeModuleType module;
-		short link;
-		short boardNum;
-		long  baseAddress;
-		long handle;
-		bool isConnected;
-		IDaqVmeCode status;
-		uint16_t       inputLevel[ 3 ];
-        uint16_t       internalReg[ 44 ];
-        CVIOPolarity   outPol[ 5 ];
-        CVLEDPolarity  outLedPol[ 5 ];
-        CVIOSources    outSource[ 5 ];
-		CVIOPolarity   inPol[ 2 ];
-		CVLEDPolarity  inLedPol[ 2 ];
-		unsigned short scaler;
+public:
+    IDaqVmeInterface( IDaqVmeModuleType aModule = idmV2718, short aLink = 0, short aBoardNum = 0, long aBaseAddress = 0 );
+    virtual ~IDaqVmeInterface();
+    
+    //Base Address
+    long GetBaseAddress(){ return m_baseAddr; };
+    void SetBaseAddress( long aBaseAddress ){ m_baseAddr = aBaseAddress; };
+    //Reset I/F
+    void Reset();
+    
+    //Link
+    void setBoardNumber( short aBoardNum = 0 ){ m_sNumBoard = aBoardNum; return; };
+    void setLinkNumber( short aLink = 0) { m_sNumLink = aLink; return; };
+    void setModuleType( IDaqVmeModuleType aModule = idmV2718 ){ m_vmeType = aModule; return; };
+    
+    // Read functions
+    //virtual void Read( uint32_t aAddress, uint32_t &aData);
+    //virtual void Read( uint32_t aAddress, uint16_t &aData);
+    template<typename RegSize_t>
+    void Read( uint32_t aAddress, RegSize_t &aData){
+        m_vmeStatus = (IDaqVmeCode)CAENVME_ReadCycle( m_lHandle, (unsigned long)aAddress, &aData, cvA32_U_DATA, cvD16);
+        return;
+    };
+    
+    //virtual void ReadBLT( uint32_t aAddress, uint16_t &aData, const uint16_t nData, uint16_t &nDataRead){};
+    //virtual void ReadBLT( uint32_t aAddress, uint32_t &aData, const uint16_t nData, uint16_t &nDataRead);
+    //virtual void ReadBLT( uint32_t aAddress, uint64_t &aData, const uint16_t nData, uint16_t &nDataRead);
+    template<typename RegSize_t>
+    void ReadBLT( uint32_t aAddress, RegSize_t &aData, const uint16_t nData, uint16_t &nDataRead){
+        int nbread( 0 );
+        m_vmeStatus = (IDaqVmeCode)CAENVME_BLTReadCycle( m_lHandle, ( unsigned long )aAddress, &aData, nData * 8, cvA32_U_MBLT, cvD64,&nbread );
+        nDataRead = nbread/ sizeof( aData );
+        return;
+    };
+    
+    // Write functions
+    //virtual void Write( uint32_t aAddress, const uint32_t &aData);
+    //virtual void Write( uint32_t aAddress, const uint16_t &aData);
+    template<typename RegSize_t>
+    void Write( uint32_t aAddress, const RegSize_t &aData){
+        m_vmeStatus = ( IDaqVmeCode ) CAENVME_WriteCycle( m_lHandle, ( unsigned long ) aAddress, ( void* ) &aData, cvA32_U_DATA, cvD16 );
+        return;
+    };
+    
+    virtual void WriteA24(uint32_t, const uint16_t&);
+    //Status
+    virtual IDaqVmeCode GetStatus(){ return m_vmeStatus; };
+    virtual void ResetStatus(){ m_vmeStatus = IDaqSuccess; };
+    
+    //Connection
+    virtual void Connect();
+    virtual void Disconnect();
+    
+    //Registers
+    virtual void CheckRegister( CVRegisters reg );
+    
+    virtual void SetOutputLevel( int aOutput, IDaqSwitch sw );
+    virtual void CheckOuputConf( int aOutput );
+    virtual void SetOutputSource( int aOutput, CVIOSources s);
+    
+    virtual void SetScalerConf();
+    virtual void ResetScaler();
+    virtual void CheckScaler();
+    virtual unsigned short GetScaler(){ return m_usScaler; };
+    virtual void EnableScaler();
+    virtual void DisableScaler();
+    
+    virtual uint16_t GetInputLevel( int aInput ) { return (m_uiInputLevel.get())[ aInput ]; }
+    virtual void CheckInputLevel();
+    virtual void SetInputConf( int aInput, bool aPolarity );
+    virtual void GetInputConf( int aInput, int &aPol, int &aLedPol ){ aPol = (m_cvInIOPol.get())[ aInput ]; aLedPol = (m_cvInLedPol.get())[ aInput ]; };
+    virtual void CheckInputConf( int aInput = 0 );
+    
+    
+private:
+    IDaqVmeModuleType m_vmeType;
+    short m_sNumLink;
+    short m_sNumBoard;
+    long  m_baseAddr;
+    long m_lHandle;
+    bool m_bisConnected;
+    IDaqVmeCode m_vmeStatus;
+    
+    /*uint16_t       inputLevel[ 3 ];
+    uint16_t       internalReg[ 44 ];
+    CVIOPolarity   outPol[ 5 ];
+    CVLEDPolarity  outLedPol[ 5 ];
+    CVIOSources    outSource[ 5 ];
+    CVIOPolarity   inPol[ 2 ];
+    CVLEDPolarity  inLedPol[ 2 ];*/
+    std::shared_ptr<uint16_t>       m_uiInputLevel;
+    std::shared_ptr<uint16_t>       m_RegInternal;
+    std::shared_ptr<CVIOPolarity>   m_cvOutIOPol;
+    std::shared_ptr<CVLEDPolarity>  m_cvOutLedPol;
+    std::shared_ptr<CVIOSources>    m_cvOutSource;
+    std::shared_ptr<CVIOPolarity>   m_cvInIOPol;
+    std::shared_ptr<CVLEDPolarity>  m_cvInLedPol;
+    unsigned short m_usScaler;
 };
 
 
--- src/IDaqVmeInterface.cpp
+++ src/IDaqVmeInterface.cpp
@@ -3,131 +3,146 @@
 using std::cerr;
 using std::endl;
 
-IDaqVmeInterface::IDaqVmeInterface( IDaqVmeModuleType aModule, short aLink, short aBoardNum, long aBaseAddress ){
-	isConnected = false;
-	handle = 0;
-	boardNum = aBoardNum;
-	link = aLink;
-	module = aModule;
+IDaqVmeInterface::IDaqVmeInterface( IDaqVmeModuleType aModule, short aLink, short aBoardNum, long aBaseAddress ) :
+        m_uiInputLevel( new uint16_t[3] ),
+        m_RegInternal( new uint16_t[44] ),
+        m_cvOutIOPol( new CVIOPolarity[5] ),
+        m_cvOutLedPol( new CVLEDPolarity[5] ),
+        m_cvOutSource( new CVIOSources[5] ),
+        m_cvInIOPol( new CVIOPolarity[2] ),
+        m_cvInLedPol( new CVLEDPolarity[2] ) {
+	m_bisConnected = false;
+	m_lHandle = 0;
+    m_baseAddr = aBaseAddress;
+	m_sNumBoard = aBoardNum;
+	m_sNumLink = aLink;
+	m_vmeType = aModule;
 }
 
 IDaqVmeInterface::~IDaqVmeInterface(){
-	if( isConnected ) this->Disconnect();
+	if( m_bisConnected ) this->Disconnect();
+    
+    m_uiInputLevel.reset();
+    m_RegInternal.reset();
+    m_cvOutIOPol.reset();
+    m_cvOutLedPol.reset();
+    m_cvOutSource.reset();
+    m_cvInIOPol.reset();
+    m_cvInLedPol.reset();
 }
 
-void IDaqVmeInterface::Read( uint32_t aAddress, uint32_t &aData){
-	status = (IDaqVmeCode)CAENVME_ReadCycle( handle, (unsigned long)aAddress, &aData, cvA32_U_DATA, cvD32);
-}
+/*void IDaqVmeInterface::Read( uint32_t aAddress, uint32_t &aData){
+	m_vmeStatus = (IDaqVmeCode)CAENVME_ReadCycle( m_lHandle, (unsigned long)aAddress, &aData, cvA32_U_DATA, cvD32);
+}*/
 
-void IDaqVmeInterface::Read( uint32_t aAddress, uint16_t &aData){
-	status = (IDaqVmeCode)CAENVME_ReadCycle( handle, (unsigned long)aAddress, &aData, cvA32_U_DATA, cvD16);
-}
+/*void IDaqVmeInterface::Read( uint32_t aAddress, uint16_t &aData){
+	m_vmeStatus = (IDaqVmeCode)CAENVME_ReadCycle( m_lHandle, (unsigned long)aAddress, &aData, cvA32_U_DATA, cvD16);
+}*/
 
 
-void IDaqVmeInterface::ReadBLT( uint32_t aAddress, uint32_t &aData, const uint16_t nData, uint16_t &nDataRead){
+/*void IDaqVmeInterface::ReadBLT( uint32_t aAddress, uint32_t &aData, const uint16_t nData, uint16_t &nDataRead){
 	int nbread(0);
-  status = (IDaqVmeCode)CAENVME_BLTReadCycle( handle, (unsigned long)aAddress, &aData, nData * 4, cvA32_U_BLT, cvD32, &nbread );
+  m_vmeStatus = (IDaqVmeCode)CAENVME_BLTReadCycle( m_lHandle, (unsigned long)aAddress, &aData, nData * 4, cvA32_U_BLT, cvD32, &nbread );
   nDataRead = nbread / sizeof( aData );
-}
+}*/
 
-void IDaqVmeInterface::ReadBLT( uint32_t aAddress, uint64_t &aData, const uint16_t nData, uint16_t &nDataRead){
+/*void IDaqVmeInterface::ReadBLT( uint32_t aAddress, uint64_t &aData, const uint16_t nData, uint16_t &nDataRead){
 	int nbread( 0 );
-  status = (IDaqVmeCode)CAENVME_BLTReadCycle( handle, ( unsigned long )aAddress, &aData, nData * 8, cvA32_U_MBLT, cvD64,&nbread );
+  m_vmeStatus = (IDaqVmeCode)CAENVME_BLTReadCycle( m_lHandle, ( unsigned long )aAddress, &aData, nData * 8, cvA32_U_MBLT, cvD64,&nbread );
   nDataRead = nbread/ sizeof( aData );
-}
+}*/
 
-void IDaqVmeInterface::Write( uint32_t aAddress, const uint32_t &aData){
-	status = ( IDaqVmeCode ) CAENVME_WriteCycle( handle, ( unsigned long ) aAddress, ( void* ) &aData, cvA32_U_DATA, cvD32 );
-}
+/*void IDaqVmeInterface::Write( uint32_t aAddress, const uint32_t &aData){
+	m_vmeStatus = ( IDaqVmeCode ) CAENVME_WriteCycle( m_lHandle, ( unsigned long ) aAddress, ( void* ) &aData, cvA32_U_DATA, cvD32 );
+}*/
 
-void IDaqVmeInterface::Write( uint32_t aAddress, const uint16_t &aData){
-  //  cout << "Writing A24 D16 ... " << endl; 
-	status = ( IDaqVmeCode ) CAENVME_WriteCycle( handle, ( unsigned long ) aAddress, ( void* ) &aData, cvA32_U_DATA, cvD16 );
-}
+/*void IDaqVmeInterface::Write( uint32_t aAddress, const uint16_t &aData){
+	m_vmeStatus = ( IDaqVmeCode ) CAENVME_WriteCycle( m_lHandle, ( unsigned long ) aAddress, ( void* ) &aData, cvA32_U_DATA, cvD16 );
+}*/
 
 void IDaqVmeInterface::WriteA24( uint32_t aAddress, const uint16_t &aData){
   //  cout << "Writing A24 D16 ... " << endl;
-  status = ( IDaqVmeCode ) CAENVME_WriteCycle( handle, ( unsigned long ) aAddress, ( void* ) &aData, cvA24_U_DATA, cvD16 );
+  m_vmeStatus = ( IDaqVmeCode ) CAENVME_WriteCycle( m_lHandle, ( unsigned long ) aAddress, ( void* ) &aData, cvA24_U_DATA, cvD16 );
 }
 
 
 void IDaqVmeInterface::Connect(){
   long BHandle;
-  if( !isConnected ){
+  if( !m_bisConnected ){
   	
   	CVBoardTypes type = cvV2718;
-  	switch( module ){
+  	switch( m_vmeType ){
   		case idmV2718: type = cvV2718; break;
   		case idmV1718: type = cvV1718; break;
   		case idmGenericModule:
   		case idmV787:
   		case idmV792:
   		case idmV792N:
-  		default: status = IDaqInvalidParam;
+  		default: m_vmeStatus = IDaqInvalidParam;
 		}  	
-		CVErrorCodes stat = CAENVME_Init( type, link, boardNum, &BHandle );
+		CVErrorCodes stat = CAENVME_Init( type, m_sNumLink, m_sNumBoard, &BHandle );
 	  if(  stat != cvSuccess )  {
   	 	std::cout << std::endl << std::endl << " Error opening the device with following parameters: " << std::endl
-  	 						<< "Module Type = " << (int)module << std::endl << "Link = " << link << std::endl 
-  	 						<< "Board Number = " << boardNum << std::endl;
-    	status = (IDaqVmeCode)stat; 
+  	 						<< "Module Type = " << (int)m_vmeType << std::endl << "Link = " << m_sNumLink << std::endl 
+  	 						<< "Board Number = " << m_sNumBoard << std::endl;
+    	m_vmeStatus = (IDaqVmeCode)stat; 
   	} else {
-	  	isConnected = true;
-  		handle = BHandle;
-  		status = IDaqSuccess;
+	  	m_bisConnected = true;
+  		m_lHandle = BHandle;
+  		m_vmeStatus = IDaqSuccess;
   	}
 	}
 }
 
 void IDaqVmeInterface::Disconnect(){
-	if( isConnected ){  
-  	status = IDaqCommError; 
-  	status = (IDaqVmeCode)CAENVME_End( handle );
-  	isConnected = false;
-  	handle = 0;
+	if( m_bisConnected ){  
+  	m_vmeStatus = IDaqCommError; 
+  	m_vmeStatus = (IDaqVmeCode)CAENVME_End( m_lHandle );
+  	m_bisConnected = false;
+  	m_lHandle = 0;
   }
 }
 
 void IDaqVmeInterface::Reset(){
-	status = (IDaqVmeCode)CAENVME_SystemReset( handle );
+	m_vmeStatus = (IDaqVmeCode)CAENVME_SystemReset( m_lHandle );
 }
 
 void IDaqVmeInterface::CheckRegister( CVRegisters reg ) {
   unsigned short data;
 	//cout << "Going to check register " << reg << endl;
-  status = (IDaqVmeCode)CAENVME_ReadRegister( handle, reg, &data );
-	//cout << " Status = " << " Data = " << data << endl;
-  if ( status == IDaqSuccess) internalReg[ reg ] = data;
+  m_vmeStatus = (IDaqVmeCode)CAENVME_ReadRegister( m_lHandle, reg, &data );
+	//cout << " m_vmeStatus = " << " Data = " << data << endl;
+  if ( m_vmeStatus == IDaqSuccess) (m_RegInternal.get())[ reg ] = data;
 	//cout << "Done." << endl;
 }
 
 void IDaqVmeInterface::CheckInputLevel(){
 	CheckRegister( cvInputReg );
-	//cout << "Checking inputs... IntReg = " << internalReg[ cvInputReg ] <<  endl;
+	//cout << "Checking inputs... IntReg = " << (m_RegInternal.get())[ cvInputReg ] <<  endl;
 	
-  if ( status == IDaqSuccess ){
-	  inputLevel[ 0 ] = ( cvIn0Bit   & internalReg[ cvInputReg ] );
-		//cout << "inputLevel[ 0 ] = " << inputLevel[ 0 ] << endl;
-  	inputLevel[ 1 ] = ( cvIn1Bit   & internalReg[ cvInputReg ] );
-  	inputLevel[ 2 ] = ( cvCoincBit & internalReg[ cvInputReg ] );
+  if ( m_vmeStatus == IDaqSuccess ){
+	  (m_uiInputLevel.get())[ 0 ] = ( cvIn0Bit   & (m_RegInternal.get())[ cvInputReg ] );
+		//cout << "(m_uiInputLevel.get())[ 0 ] = " << (m_uiInputLevel.get())[ 0 ] << endl;
+  	(m_uiInputLevel.get())[ 1 ] = ( cvIn1Bit   & (m_RegInternal.get())[ cvInputReg ] );
+  	(m_uiInputLevel.get())[ 2 ] = ( cvCoincBit & (m_RegInternal.get())[ cvInputReg ] );
 	} else { cerr << "Couldn't check the input level!" << endl; }
 }
 
 void IDaqVmeInterface::CheckInputConf( int aInput ){
-	status = (IDaqVmeCode) CAENVME_GetInputConf( handle, (CVInputSelect)aInput,  &inPol[ aInput ], &inLedPol[ aInput ] );
+	m_vmeStatus = (IDaqVmeCode) CAENVME_GetInputConf( m_lHandle, (CVInputSelect)aInput,  &(m_cvInIOPol.get())[ aInput ], &(m_cvInLedPol.get())[ aInput ] );
 }
 
 void IDaqVmeInterface::SetInputConf( int aInput, bool aPolarity ){
 	
-	status = (IDaqVmeCode) CAENVME_SetInputConf( handle, ( CVInputSelect ) aInput, (CVIOPolarity) aPolarity, (CVLEDPolarity) aPolarity );
-	if( status == IDaqSuccess ){
+	m_vmeStatus = (IDaqVmeCode) CAENVME_SetInputConf( m_lHandle, ( CVInputSelect ) aInput, (CVIOPolarity) aPolarity, (CVLEDPolarity) aPolarity );
+	if( m_vmeStatus == IDaqSuccess ){
 		CheckInputConf( aInput );
 	}
 	/*
-	status = (IDaqVmeCode)CAENVME_SetInputConf( handle, cvInput0, cvInverted, cvActiveLow );
-	status = (IDaqVmeCode)CAENVME_SetInputConf( handle, cvInput1, cvInverted, cvActiveLow );
+	m_vmeStatus = (IDaqVmeCode)CAENVME_SetInputConf( m_lHandle, cvInput0, cvInverted, cvActiveLow );
+	m_vmeStatus = (IDaqVmeCode)CAENVME_SetInputConf( m_lHandle, cvInput1, cvInverted, cvActiveLow );
 	
-	if( status == IDaqSuccess ){
+	if( m_vmeStatus == IDaqSuccess ){
 		CheckInputConf( aInput );
 	}
 	*/
@@ -137,48 +152,48 @@ void IDaqVmeInterface::SetOutputLevel(int aOutput, IDaqSwitch sw ){
   unsigned short dc = 0x1 << ( 6 + aOutput );
   switch ( sw ) {
   case IDaqEnable:
-    status = ( IDaqVmeCode ) CAENVME_SetOutputRegister( handle, dc );
+    m_vmeStatus = ( IDaqVmeCode ) CAENVME_SetOutputRegister( m_lHandle, dc );
     break;
   case IDaqDisable:
-    status = (IDaqVmeCode)CAENVME_ClearOutputRegister( handle, dc );
+    m_vmeStatus = (IDaqVmeCode)CAENVME_ClearOutputRegister( m_lHandle, dc );
     break;
   default:
-    status = IDaqInvalidParam;
+    m_vmeStatus = IDaqInvalidParam;
     break;
   }
 }
 
 void IDaqVmeInterface::CheckOuputConf( int aOutput ){
-     status = (IDaqVmeCode) CAENVME_GetOutputConf( handle, (CVOutputSelect)aOutput, &outPol[ aOutput ], &outLedPol[ aOutput ], &outSource[ aOutput ] );
+     m_vmeStatus = (IDaqVmeCode) CAENVME_GetOutputConf( m_lHandle, (CVOutputSelect)aOutput, &(m_cvOutIOPol.get())[ aOutput ], &(m_cvOutLedPol.get())[ aOutput ], &(m_cvOutSource.get())[ aOutput ] );
 }
 
 
 void IDaqVmeInterface::SetOutputSource( int aOutput, CVIOSources s){
 	CheckOuputConf( aOutput );
-  if( status == IDaqSuccess ){
-     status = (IDaqVmeCode) CAENVME_SetOutputConf( handle, ( CVOutputSelect ) aOutput, outPol[ aOutput ], outLedPol[ aOutput ], s );
+  if( m_vmeStatus == IDaqSuccess ){
+     m_vmeStatus = (IDaqVmeCode) CAENVME_SetOutputConf( m_lHandle, ( CVOutputSelect ) aOutput, (m_cvOutIOPol.get())[ aOutput ], (m_cvOutLedPol.get())[ aOutput ], s );
 	}
 }
 
 void IDaqVmeInterface::SetScalerConf(){
-	status = (IDaqVmeCode) CAENVME_SetScalerConf( handle, 1024, 0, cvInputSrc0, cvManualSW, cvManualSW );
+	m_vmeStatus = (IDaqVmeCode) CAENVME_SetScalerConf( m_lHandle, 1024, 0, cvInputSrc0, cvManualSW, cvManualSW );
 }
 
 void IDaqVmeInterface::ResetScaler(){
-	status = (IDaqVmeCode) CAENVME_ResetScalerCount( handle );
+	m_vmeStatus = (IDaqVmeCode) CAENVME_ResetScalerCount( m_lHandle );
 }
 
 void IDaqVmeInterface::EnableScaler(){
-	status = (IDaqVmeCode) CAENVME_EnableScalerGate( handle );
+	m_vmeStatus = (IDaqVmeCode) CAENVME_EnableScalerGate( m_lHandle );
 }
 
 void IDaqVmeInterface::DisableScaler(){
-	status = (IDaqVmeCode) CAENVME_DisableScalerGate( handle );
+	m_vmeStatus = (IDaqVmeCode) CAENVME_DisableScalerGate( m_lHandle );
 }
 
 void IDaqVmeInterface::CheckScaler(){
 	CheckRegister( cvScaler1 );
-	if( status == IDaqSuccess ){
-		scaler = internalReg[ cvScaler1 ];
-	} else { scaler = 6666; }
+	if( m_vmeStatus == IDaqSuccess ){
+		m_usScaler = (m_RegInternal.get())[ cvScaler1 ];
+	} else { m_usScaler = 6666; }
 }
