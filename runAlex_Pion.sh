b8aa3b95d56f8e49e02a49eb9bef136d00c28d40
Brian Dorney <brian.l.dorney@cern.ch>
2016-09-27 14:24:34 +0200

    merged feature_EvtRecoDigi and cleaned up repo

--- runAlex_Pion.sh
+++
@@ -1,8 +0,0 @@
-startRun=$1
-for i in {0..7}
-do
-  ./Linux/TestTDC << EOF
-    GE11_TDCRun$((startRun+i))_Pion_5k_MSPL4_Async_$2uA_T$3_T$4_2015 
-EOF
-
-done
--- test/make_setscaler.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ setscaler.cc -I../include -I../include/CAEN -o setscaler -Wall -Dlong32="int" -Dlong64="long long" -L../lib -lCAENVME
